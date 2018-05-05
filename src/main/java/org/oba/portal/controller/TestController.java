c351bcd589f180dfc0ee1382615ab2ef16ab0097
Sanal Oba System <36585397+SanalObaSystem@users.noreply.github.com>
2018-05-05 11:19:51 +0300

    Test message is updated!

--- src/main/java/org/oba/portal/controller/TestController.java
+++ src/main/java/org/oba/portal/controller/TestController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RestController
 public class TestController {
-    @GetMapping("/ping")	//	standarduser
-    public String ping() {
-        return "OK!";
+    @GetMapping("/hello")	//	standarduser
+    public String hello() {
+        return "Hello!";
     }
 
     @PreAuthorize("hasAnyRole('USERMAN', 'GROUPUSERMAN')")	//	admin
